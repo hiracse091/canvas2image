6d475d839f4594e7e9a4bedb67aac1cf2d1b34bd
Naher <hiracse091@gmail.com>
2015-10-30 18:02:52 +0600

    init

---
+++ www/Canvas2ImagePlugin.js
@@ -0,0 +1,27 @@
+//
+//  Canvas2ImagePlugin.js
+//  Canvas2ImagePlugin PhoneGap/Cordova plugin
+//
+//  Created by Tommy-Carlos Williams on 29/03/12.
+//  Copyright (c) 2012 Tommy-Carlos Williams. All rights reserved.
+//  MIT Licensed
+//
+
+  module.exports = {
+    
+    saveImageDataToLibrary:function(successCallback, failureCallback, data) {
+        // successCallback required
+        if (typeof successCallback != "function") {
+            console.log("Canvas2ImagePlugin Error: successCallback is not a function");
+        }
+        else if (typeof failureCallback != "function") {
+            console.log("Canvas2ImagePlugin Error: failureCallback is not a function");
+        }
+        else {
+            //var canvas = (typeof data[0] === "string") ? document.getElementById(data[0]) : data[0];
+            var imageData = data[0];//canvas.toDataURL().replace(/data:image\/png;base64,/,'');
+            return cordova.exec(successCallback, failureCallback, "Canvas2ImagePlugin","saveImageDataToLibrary",[imageData,data[1]]);
+        }
+    }
+  };
+  
